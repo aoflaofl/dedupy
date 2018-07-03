187941725637e81ebb08d3514ba18e1a12525fa6
Gene Johannsen <aofl@yahoo.com>
2018-07-03 10:39:03 -0700

    Changes due to lint and formatting

--- src/dedupe.py
+++ src/dedupe.py
@@ -1,26 +1,38 @@
+"""
+Adapted from a sample script by Randall Hettinger.
+"""
+
 import hashlib
 import os
 from pprint import pprint
 
 
 def group_files_by_size(start_dir=".", ignore_zero_len=True):
-
-    # Search a directory tree for all files with duplicate content
+    """
+    Create a dict of files mapped to size.  By default ignore zero length files.
+    """
     size_filenames_dict = {}  # content signature -> list of matching filenames
 
     for path, _, files in os.walk(start_dir):
         for filename in files:
             fullname = os.path.join(path, filename)
-            file_size = os.stat(fullname).st_size
-            if ignore_zero_len == True and file_size == 0:
-                continue
+            try:
+                file_size = os.stat(fullname).st_size
 
-            size_filenames_dict.setdefault(file_size, []).append(fullname)
+                if ignore_zero_len is True and file_size == 0:
+                    continue
+
+                size_filenames_dict.setdefault(file_size, []).append(fullname)
+            except (PermissionError, FileNotFoundError):
+                pass
 
     return size_filenames_dict
 
 
 def hash_list_of_files(list_of_files):
+    """
+    Take a list of files and group them by a hash function.
+    """
     hash_files = {}
     for filename in list_of_files:
         try:
@@ -35,7 +47,7 @@ def hash_list_of_files(list_of_files):
     return hash_files
 
 
-dic = group_files_by_size("C:/")
+dic = group_files_by_size("/")
 for size, file_list in dic.items():
     if len(file_list) > 1:
         # pprint(size)
